0b36b9d24f545ff7808b8175935576f5240918bb
Md Ashik Ali <mdashika989@yahoo.con>
2021-07-14 21:23:17 +0600

    some thing kisu ekta

--- js/index.js
+++ js/index.js
@@ -7,9 +7,9 @@ let header = document.querySelector('#header');
 
 window.onscroll = function(){
   let top = window.scrollY;
-  if(top >= 150){
-    header.classList.add('scrolled');
+  if(top < 150){
+    header.classList.remove('scrolled');
   }else{
-    header.classList.remove('scrolled')
+    header.classList.add('scrolled')
   }
 }
